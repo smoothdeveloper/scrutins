21503c83a5acd15d9bb8ec9385e475bd884ada7e
lucas <lucas.gautheron@gmail.com>
2017-02-27 01:56:55 +0100

    parser

--- get_election_data.py
+++ get_election_data.py
@@ -2,6 +2,7 @@ import csv
 import urllib
 import os.path
 import shutil
+import json
 
 def download_page(url, destination):
     if os.path.isfile(destination):
@@ -14,7 +15,8 @@ def fix_csv(file, original_delim, desired_delim):
     newLogFile = open("tmp", 'w')
     textFile = open(file, 'rb')
     for row in textFile:
-        newLogFile.write(desired_delim.join(row.split(original_delim)))
+        row.replace(original_delim, desired_delim)
+        newLogFile.write(row)
 
     shutil.copy2("tmp", file)
 
@@ -22,7 +24,18 @@ download_page("https://www.data.gouv.fr/s/resources/election-presidentielle-2012
 download_page("https://www.data.gouv.fr/s/resources/referendum-de-2005-resultats-par-bureaux-de-vote/20150925-112918/RF05_BVot.txt", "data/2005.csv")
 
 fix_csv("data/2005.csv", ",", ";")
-# parse
+fix_csv("data/pres_2012.csv", ",", ";")
+
+def init_res():
+    c = {}
+    c["exprimes_2005"] = 0
+    c["votants_2005"] = 0
+    c["abstention_2005"] = 0
+    c["inscrits_2005"] = 0
+    c["exprimes_2012"] = 0
+    c["votants_2012"] = 0
+    c["inscrits_2012"] = 0
+    return c
 
 bureaux = []
 communes = {}
@@ -39,12 +52,10 @@ with open('data/2005.csv', 'rb') as csvfile:
         resultat = row[14].strip()
 
         if not insee in communes:
-            communes[insee] = {}
-            communes[insee]["OUI_2005"] = 0
-            communes[insee]["NON_2005"] = 0
-            communes[insee]["exprimes_2005"] = 0
-            communes[insee]["votants_2005"] = 0
-            communes[insee]["abstention_2005"] = 0
+            communes[insee] = init_res()
+
+        if not resultat + "_2005" in communes[insee]:
+            communes[insee][resultat + "_2005"] = 0
 
         communes[insee][resultat + "_2005"] = communes[insee][resultat + "_2005"] + int(row[15])
 
@@ -53,7 +64,35 @@ with open('data/2005.csv', 'rb') as csvfile:
             communes[insee]["votants_2005"] = communes[insee]["votants_2005"] + int(row[11])
             communes[insee]["abstention_2005"] = communes[insee]["abstention_2005"] + int(row[12])
 
+with open('data/pres_2012.csv', 'rb') as csvfile:
+    reader = csv.reader(csvfile, delimiter=';', quotechar='|')
+    for row in reader:
+        if len(row) != 15:
+            print "ignored", ','.join(row)
+            continue
+
+        if int(row[0]) != 1:
+            continue
+
+        dpt = row[1]
+        insee = row[1] + row[2]
+        resultat = row[13].strip()
+
+        if not insee in communes:
+            communes[insee] = init_res()
+
+        if not resultat + "_2012" in communes[insee]:
+            communes[insee][resultat + "_2012"] = 0
+
+        communes[insee][resultat + "_2012"] = communes[insee][resultat + "_2012"] + int(row[14])
+
+        if resultat == "HOLL":
+            communes[insee]["exprimes_2012"] = communes[insee]["exprimes_2012"] + int(row[9])
+            communes[insee]["votants_2012"] = communes[insee]["votants_2012"] + int(row[8])
+            communes[insee]["inscrits_2012"] = communes[insee]["inscrits_2012"] + int(row[7])
+
         #print insee, dpt
+for insee,commune in communes.iteritems():
+    print insee
 
-
-
+open("communes.json", 'w').write(json.dumps(communes, indent=4))
