7d2ee9af546f5da2daccc71f5f382b394436c1e3
Arthur Cheysson <arthur@cheysson.fr>
2017-03-26 15:09:49 +0200

    Ajoute les élections législatives de 2012
    Les données produites devraient être validées, compte tenu des
    spécificités des législatives par rapport aux élections nationales,
    notamment l'absence éventuelle de seconds tours en cas de victoire
    au premier, l'absence de certaines nuances dans certaines circos,
    et sans doute d'autres.

--- get_election_data.py
+++ get_election_data.py
@@ -10,7 +10,7 @@ from json import encoder
 encoder.FLOAT_REPR = lambda o: format(o, '.2f')
 
 
-def calculer(df):
+def calculer_totaux(df):
     stats_index = ['departement', 'commune_code', 'tour']
     choix_index = stats_index + ['choix']
 
@@ -35,10 +35,14 @@ def calculer(df):
             .groupby(level=stats_index).sum()
             # puis on dépile le numéro de tour et on le met en premier index de colonne
             .unstack(['tour']).swaplevel(0, 1, axis=1).sortlevel(axis=1)
+            # enfin, on remplace les valeurs manquantes avec des 0 (pour les élections sans second tour)
+            .fillna(0, downcast='infer')
     )
     stats.columns = stats.columns.set_names(['tour', 'statistique'])
 
-    choix = df.groupby(choix_index)['voix'].sum().unstack(['tour', 'choix'])
+    # le fillna est utilisé pour les législatives : toutes les nuances ne sont pas présentes dans toutes
+    # les circos, donc il faut remplacer les valeurs manquantes par des 0, et recaster en int
+    choix = df.groupby(choix_index)['voix'].sum().unstack(['tour', 'choix']).fillna(0, downcast='infer').sortlevel(axis=1)
 
     # on vérifie que le nombre de suffrages exprimés est égal à la somme des votes pour chaque choix, et ce pour chaque
     # tour de l'élection
@@ -47,6 +51,21 @@ def calculer(df):
     ).all().all()
 
     return stats, choix
+
+
+def calculer_scores(stats, choix, nonistes_gauche, nonistes_droite):
+    scores = 100 * choix[1].divide(stats[1]['inscrits'], axis=0)
+    scores['NONISTES_DROITE'] = scores[nonistes_droite].sum(axis=1)
+    scores['NONISTES_GAUCHE'] = scores[nonistes_gauche].sum(axis=1)
+    scores['NONISTES'] = scores['NONISTES_DROITE'] + scores['NONISTES_GAUCHE']
+    return scores
+
+
+use_columns = [
+    'tour', 'departement', 'commune_code', 'bureau',
+    'inscrits', 'votants', 'exprimes',
+    'choix', 'voix'
+]
 
 
 # Pour 2005
@@ -58,12 +77,13 @@ df_2005 = pd.read_csv(
     encoding='cp1252',
     names=['tour', 'region', 'departement', 'arrondissement', 'circo', 'canton', 'commune_code', 'ref_inscrits',
            'commune_nom', 'bureau', 'inscrits', 'votants', 'abstentions', 'exprimes', 'choix', 'voix'],
-    dtype={'departement': str, 'commune_code': str, 'bureau': str}
+    dtype={'departement': str, 'commune_code': str, 'bureau': str},
+    usecols=use_columns
 )
 # attention aux espaces en trop dans la réponse
 df_2005['choix'] = df_2005.choix.str.strip()
 
-stats_2005, choix_2005 = calculer(df_2005)
+stats_2005, choix_2005 = calculer_totaux(df_2005)
 
 # 2007 maintenant
 
@@ -74,46 +94,71 @@ df_2007 = pd.read_csv(
     encoding='cp1252',
     names=['tour', 'departement', 'commune_code', 'commune_nom', 'bureau', 'inscrits', 'votants', 'exprimes',
            'numero_candidat', 'nom_candidat', 'prenom_candidat', 'choix', 'voix'],
-    dtype={'departement': str, 'commune_code': str, 'bureau': str}
+    dtype={'departement': str, 'commune_code': str, 'bureau': str},
+    usecols=use_columns
 )
 
-stats_2007, choix_2007 = calculer(df_2007)
+stats_2007, choix_2007 = calculer_totaux(df_2007)
 
-df_2012 = pd.read_csv(
+df_pres_2012 = pd.read_csv(
     'data/pres_2012.csv',
     sep=';',
     encoding='cp1252',
     names=['tour', 'departement', 'commune_code', 'commune_nom', '?', '??', 'bureau', 'inscrits', 'votants', 'exprimes',
            'numero_candidat', 'nom_candidat', 'prenom_candidat', 'choix', 'voix'],
-    dtype={'departement': str, 'commune_code': str, 'bureau': str}
+    dtype={'departement': str, 'commune_code': str, 'bureau': str},
+    usecols=use_columns
 )
-stats_2012, choix_2012 = calculer(df_2012)
+stats_2012, choix_2012 = calculer_totaux(df_pres_2012)
+
+
+df_legi_2012 = pd.read_csv(
+    'data/legi_2012.csv',
+    sep=';',
+    skiprows=18,
+    names=['tour', 'departement', 'commune_code', 'commune_nom', '?', '??', 'bureau', 'inscrits', 'votants', 'exprimes',
+           'numero_candidat', 'nom_candidat', 'prenom_candidat', 'choix', 'voix'],
+    dtype={'departement': str, 'commune_code': str, 'bureau': str},
+    usecols=use_columns
+)
+stats_legi_2012, choix_legi_2012 = calculer_totaux(df_legi_2012)
+
 
 # statistiques tce
 scores_tce = pd.DataFrame({
     'OUI_TCE': 100 * choix_2005[1]['OUI'] / stats_2005[1]['inscrits'],
     'NON_TCE': 100 * choix_2005[1]['NON'] / stats_2005[1]['inscrits']
 })
-# statistiques 2012
+
+# statistiques présidentielles 2012
 nonistes_droite_2012 = ["LEPE", "DUPO"]
 nonistes_gauche_2012 = ["MELE", "ARTH", "POUT"]
-scores_pres_2012 = 100 * choix_2012[1].divide(stats_2012[1]['inscrits'], axis=0)
-scores_pres_2012['NONISTES_DROITE'] = scores_pres_2012[nonistes_droite_2012].sum(axis=1)
-scores_pres_2012['NONISTES_GAUCHE'] = scores_pres_2012[nonistes_gauche_2012].sum(axis=1)
-scores_pres_2012['NONISTES'] = scores_pres_2012['NONISTES_DROITE'] + scores_pres_2012['NONISTES_GAUCHE']
+scores_pres_2012 = calculer_scores(stats_2012, choix_2012,
+                                   nonistes_droite=nonistes_droite_2012, nonistes_gauche=nonistes_gauche_2012)
 
-# statistiques 2007
+# statistiques présidentielles 2007
 nonistes_droite_2007 = ["LEPE", "NIHO", "VILL"]
 nonistes_gauche_2007 = ["BUFF", "BESA", "SCHI"]
-scores_pres_2007 = 100 * choix_2007[1].divide(stats_2007[1]['inscrits'], axis=0)
-scores_pres_2007['NONISTES_DROITE'] = scores_pres_2007[nonistes_droite_2007].sum(axis=1)
-scores_pres_2007['NONISTES_GAUCHE'] = scores_pres_2007[nonistes_gauche_2007].sum(axis=1)
-scores_pres_2007['NONISTES'] = scores_pres_2007['NONISTES_DROITE'] + scores_pres_2007['NONISTES_GAUCHE']
+scores_pres_2007 = calculer_scores(stats_2007, choix_2007,
+                                   nonistes_droite=nonistes_droite_2007, nonistes_gauche=nonistes_gauche_2007)
+
+
+# statistiques législatives 2012
+# qui des divers droite ? Beaucoup doivent être nonistes
+# sans doute moins le cas pour les divers gauche.
+nonistes_droite_legislatives_2012 = ['FN', 'EXD']
+nonistes_gauche_legislatives_2012 = ['FG', 'EXG']
+scores_legi_2012 = calculer_scores(stats_legi_2012, choix_legi_2012,
+                                   nonistes_droite=nonistes_droite_legislatives_2012,
+                                   nonistes_gauche=nonistes_gauche_legislatives_2012)
+
+
 
 df_communes = pd.concat([
     scores_tce,
     scores_pres_2012.rename(columns=lambda c: c + '_PRES_2012'),
-    scores_pres_2007.rename(columns=lambda c: c + '_PRES_2007')
+    scores_pres_2007.rename(columns=lambda c: c + '_PRES_2007'),
+    scores_legi_2012.rename(columns=lambda c: c + '_LEGI_2012')
 ], axis=1)
 
 # a améliorer, on pourrait sortir directement du XML par exemple
